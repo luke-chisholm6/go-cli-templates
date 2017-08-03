ee1fd3b62ec7b3aa2424d28839876e261fb14964
Luke Chisholm <luke.chisholm6@gmail.com>
2017-08-03 00:21:53 -0700

    refractored into some functions

--- mustache.go
+++ mustache.go
@@ -5,27 +5,55 @@ import (
 	"os"
 	"strings"
 	"text/template"
+	"errors"
 )
 
-func main() {
-	argsWithoutProg := os.Args[1:]
+func splitKV(keyValue string) (key string, value string, err error) {
+	keyValueArray := strings.Split(keyValue, "=")
 
-	templateContext := make(map[string]string)
-
-	for _, arg := range argsWithoutProg {
-		parsedArg := strings.Split(arg, "=")
-		templateContext[parsedArg[0]] = parsedArg[1]
+	if len(keyValueArray) != 2 {
+		err = errors.New("bad formatting kv pair")
+		return
 	}
 
+	key, value = keyValueArray[0], keyValueArray[1]
+
+	return
+}
+
+func ExtractKeyValueFromArray(keyValueArray[]string) (map[string]string, error) {
+	keyValueMap := make(map[string]string)
+
+	for _, keyValue := range keyValueArray {
+		key, value, err := splitKV(keyValue)
+		if err != nil {
+			return keyValueMap, err
+		}
+		keyValueMap[key] = value
+	}
+
+	return keyValueMap, nil
+}
+
+func main() {
 	bytes, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
+	switch {
+	case err != nil:
 		panic(err)
+	case len(bytes) <= 0:
+		panic("no template context given")
 	}
 	inputTemplate := string(bytes)
-
 	compiledTemplate, err := template.New("").Parse(inputTemplate)
 	if err != nil {
 		panic(err)
 	}
+
+	templateContext, err := ExtractKeyValueFromArray(os.Args[1:])
+	if err != nil {
+		panic(err)
+	}
+
+
 	compiledTemplate.Execute(os.Stdout, templateContext)
 }
